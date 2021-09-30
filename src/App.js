ea45e874267d0143cf05f2f51e11592d16a3790e
johnmchale <38525955+johnmchale@users.noreply.github.com>
2021-09-30 22:54:18 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Calendar } from "@progress/kendo-react-dateinputs";
+import "@progress/kendo-theme-default/dist/all.css";
 import "./App.css";
 
 const App = () => {
+  const [date, setDate] = useState(new Date());
+
+  const handleChange = (event) => {
+    console.log(event.value);
+    setDate(event.value);
+  };
+
   return (
     <div>
-      {/* <Calendar value={date} onChange={handleChange} />; */}
-      <h1>Selected date: </h1>
+      <Calendar value={date} onChange={handleChange} />;
+      <h1>Selected date: {date?.toDateString()}</h1>
     </div>
   );
 };
